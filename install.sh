cff691a2f4b532e493af0b2da73840b058d9948b
Your Name <you@example.com>
2017-10-29 12:05:03 +0100

    test

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 #sudo apt-get install vlc
 sudo apt-get install git
 sudo apt-get install apache2
-sudo apt-get install curl
+sudo apt-get install php-curl
 sudo apt install mysql-server
 sudo apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip
 sudo apt-get install phpmyadmin
@@ -24,6 +24,8 @@ sudo npm install npm@latest -g
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
+
+
 
 #install jdownloader
 #mkdir ./tmp

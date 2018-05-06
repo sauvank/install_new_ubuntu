668b309380922dd64d73d417be77800948c65ee8
sauvank <kevin.sauvanaud@epitech.eu>
2018-05-06 11:26:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -75,3 +75,6 @@ echo "exec zsh" >> ~/.bashrc
 source ~/.zshrc
 chsh -s `which zsh`
 . ~/.zshrc
+
+# remove mysql
+# sudo service mysql stop && sudo killall -9 mysql && sudo killall -9 mysqld && sudo apt-get remove --purge mysql-server mysql-client mysql-common && sudo apt-get autoremove && sudo apt-get autoclean && sudo deluser mysql && sudo rm -rf /var/lib/mysql && sudo apt-get purge mysql-server-core-5.7 && sudo apt-get purge mysql-client-core-5.7 && sudo rm -rf /var/log/mysql && sudo rm -rf /etc/mysql

bf5f58e90c7391b9a9722867474c393245a0b2e6
sauvank <kevin.sauvanaud@epitech.eu>
2018-08-09 16:41:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -73,13 +73,13 @@ exec zsh
 
 
 
-/***
-CREATE USER 'monty'@'localhost' IDENTIFIED BY 'some_pass';
-GRANT ALL PRIVILEGES ON *.* TO 'monty'@'localhost' WITH GRANT OPTION;
-CREATE USER 'monty'@'%' IDENTIFIED BY 'some_pass';
-GRANT ALL PRIVILEGES ON *.* TO 'monty'@'%' WITH GRANT OPTION;
-CREATE USER 'admin'@'localhost';
-GRANT RELOAD,PROCESS ON *.* TO 'admin'@'localhost';
-CREATE USER 'dummy'@'localhost';
-FLUSH PRIVILEGES;
+#
+#CREATE USER 'monty'@'localhost' IDENTIFIED BY 'some_pass';
+#GRANT ALL PRIVILEGES ON *.* TO 'monty'@'localhost' WITH GRANT OPTION;
+#CREATE USER 'monty'@'%' IDENTIFIED BY 'some_pass';
+#GRANT ALL PRIVILEGES ON *.* TO 'monty'@'%' WITH GRANT OPTION;
+#CREATE USER 'admin'@'localhost';
+#GRANT RELOAD,PROCESS ON *.* TO 'admin'@'localhost';
+#CREATE USER 'dummy'@'localhost';
+#FLUSH PRIVILEGES;
 ***/

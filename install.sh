76da3e64e981f5a153527057970b1b9f8355669e
sauvank <kevin.sauvanaud@epitech.eu>
2018-08-09 16:41:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -68,3 +68,18 @@ echo -e "login    : phpmyadmin" &&
 echo -e "password : root (your passwrod set pending install )" &&
 source ~/.zshrc &&
 exec zsh
+
+
+
+
+
+/***
+CREATE USER 'monty'@'localhost' IDENTIFIED BY 'some_pass';
+GRANT ALL PRIVILEGES ON *.* TO 'monty'@'localhost' WITH GRANT OPTION;
+CREATE USER 'monty'@'%' IDENTIFIED BY 'some_pass';
+GRANT ALL PRIVILEGES ON *.* TO 'monty'@'%' WITH GRANT OPTION;
+CREATE USER 'admin'@'localhost';
+GRANT RELOAD,PROCESS ON *.* TO 'admin'@'localhost';
+CREATE USER 'dummy'@'localhost';
+FLUSH PRIVILEGES;
+***/
